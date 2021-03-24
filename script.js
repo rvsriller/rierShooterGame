996da1989eea4f00254a118e0f7f7d829b75260f
Riller <rvsriller@gmail.com>
2021-03-24 17:40:49 -0300

    Move down function

--- script.js
+++ script.js
@@ -19,4 +19,30 @@ function flyShip(event) {
     }
 } 
 
+//This function allows the ship to go up
+function moveUp() {
+    let topPosition = getComputedStyle(yourShip).getPropertyValue('top');
+    if(topPosition === "0px") {
+        return
+    } else {
+        let position = parseInt(topPosition);
+        position -= 50;
+        yourShip.style.top = `${position}px`;
+    }
+}
+
+
+//This function allows the ship to go down
+function moveDown() {
+    let topPosition = getComputedStyle(yourShip).getPropertyValue('top');
+    if(topPosition === "510px"){
+        return
+    } else {
+        let position = parseInt(topPosition);
+        position += 50;
+        yourShip.style.top = `${position}px`;
+    }
+}
+
+
 
