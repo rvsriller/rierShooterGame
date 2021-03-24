69bd3a1d852740f063ef671512e92377cf9a1d14
Riller <rvsriller@gmail.com>
2021-03-24 17:38:51 -0300

    Fly ship function

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-alert("Script working");
+const yourShip = document.querySelector('.playerShooter');
+const playArea = document.querySelector('#mainPlayArea');
+const aliensImg = ['img/firstmonster.png', 'img/secondmonster.png', 'img/thirdmonster.png'];
+const instructionsText = document.querySelector('.gameInstructions');
+const startButton = document.querySelector('.startButton');
+let alienInterval;
+
+//Move and shoot of ship
+function flyShip(event) {
+    if(event.key === 'ArrowUp') {
+        event.preventDefault();
+        moveUp();
+    } else if(event.key === 'ArrowDown') {
+        event.preventDefault();
+        moveDown();
+    } else if(event.key === " ") {
+        event.preventDefault();
+        fireLaser();
+    }
+} 
+
+
